e760ef7c45dc862e343cb21c25042e922bcbb976
striverz <striverzarc@gmail.com>
2025-03-17 12:30:20 +0530

    abstraction file is added

--- src/_05Abstraction/_03StaticMethod.java
+++ src/_05Abstraction/_03StaticMethod.java
@@ -1,31 +1,17 @@
 package _05Abstraction;
-class StaticExample{
 
-    static int priceOfHonda=1000;
-
-    static{
-        System.out.println("The Static block1");
+interface Vehicle {
+    static void service() {
+        System.out.println("Vehicle service in progress...");
     }
-    static {
-        System.out.println("The Static block2");
-    }
-
-
-    static void method(){
-        System.out.println("The static method in Java");
-    }
-
-    static{
-        System.out.println("The Static block3");
-    }
-
 }
-public class _03StaticMethod {
+
+class Car implements Vehicle {
+    // No need to implement static methods from the interface
+}
+
+class _03StaticMethod {
     public static void main(String[] args) {
-
-        System.out.println(StaticExample.priceOfHonda);
-        StaticExample.method();
-
-
+        Vehicle.service(); // Output: Vehicle service in progress...
     }
 }
--- src/_05Abstraction/_04DefaultMethod.java
+++ src/_05Abstraction/_04DefaultMethod.java
@@ -1,17 +1,28 @@
 package _05Abstraction;
-interface Example{
-    default void defaultMethod(){
-        System.out.println("The Default method in Interface");
+interface Vehicle1 {
+    // Default method with implementation
+    default void start() {
+        System.out.println("Vehicle is starting...");
     }
 }
-class Demo implements Example{
-    //for accessing the default method we use this
+
+class Car1 implements Vehicle1 {
+    // Inheriting the default method
 }
-public class _04DefaultMethod {
+
+class Bike implements Vehicle1 {
+    // Overriding the default method
+    public void start() {
+        System.out.println("Bike is starting...");
+    }
+}
+
+class _04DefaultMethod {
     public static void main(String[] args) {
+        Vehicle1 car = new Car1();
+        car.start(); // Output: Vehicle is starting...
 
-        Example e=new Demo();
-        e.defaultMethod();
-
+        Vehicle1 bike = new Bike();
+        bike.start(); // Output: Bike is starting...
     }
 }
